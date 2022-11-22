41aafdf8d4f95b697eef555ffd100324d611bde1
ivoserra <herrserra@icloud.com>
2022-11-22 09:38:57 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> >
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <?php wp_head(); ?>
+  
+    <title>Document</title>
+</head>
+<body <?php body_class('example');?>>
+    <?php wp_body_open(); ?>
+    <p>hello!</p>
+    <?php wp_footer(); ?>
+</body>
+</html>
